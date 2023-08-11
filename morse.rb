21b1155caaa283d0824f0cee5267c71f844ca4a6
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-11 09:55:55 -0600

    Add styling with ruby rules

--- morse.rb
+++ morse.rb
@@ -1,33 +1,23 @@
 MORSE_CODE = {
-  '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
-  '-..' => 'D', '.'     => 'E', '..-.'  => 'F',
-  '--.' => 'G', '....'  => 'H', '..'    => 'I',
-  '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-  '--' => 'M', '-.' => 'N', '---' => 'O',
-  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
-  '...' => 'S', '-' => 'T', '..-' => 'U',
-  '...-' => 'V', '.--'   => 'W', '-..-' => 'X',
-  '-.--' => 'Y', '--..'  => 'Z'
+  '.-'   => 'A',   '-...' => 'B',   '-.-.' => 'C',
+  '-..'  => 'D',   '.'    => 'E',   '..-.' => 'F',
+  '--.'  => 'G',   '....' => 'H',   '..'   => 'I',
+  '.---' => 'J',   '-.-'  => 'K',   '.-..' => 'L',
+  '--'   => 'M',   '-.'   => 'N',   '---'  => 'O',
+  '.--.' => 'P',   '--.-' => 'Q',   '.-.'  => 'R',
+  '...'  => 'S',   '-'    => 'T',   '..-'  => 'U',
+  '...-' => 'V',   '.--'  => 'W',   '-..-' => 'X',
+  '-.--' => 'Y',   '--..' => 'Z'
 }.freeze
 
 def decode_char(char)
   puts MORSE_CODE[char].upcase
 end
 
-def decode_word(string)
-  text = []
-  word = string.split
-  word.each do |char|
-    MORSE_CODE.each do |key, value|
-      text << value if key == char
-    end
-  end
-  puts text.join
-end
-
 def morse_code(string)
   decoded_text = []
   words = string.split('   ')
+  
   words.each do |word|
     new_word = word.split
     new_word.each do |single_word|
@@ -36,7 +26,21 @@ def morse_code(string)
     end
     decoded_text << ' '
   end
+  
   puts decoded_text.join
+end
+
+def decode_word(string)
+  text = []
+  word = string.split
+  
+  word.each do |char|
+    MORSE_CODE.each do |key, value|
+      text << value if key == char
+    end
+  end
+  
+  puts text.join
 end
 
 decode_char('-.')
@@ -44,3 +48,6 @@ decode_char('-.')
 morse_code('-- -.--   -. .- -- .   .. ...   -.. .- -. .. . .-..')
 
 decode_word('-.. .- -. .. . .-..')
+
+morse_code('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+
