febe6d00355b47c5c2bfc15ca75c09a9295c038d
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-11 10:19:26 -0600

    Fix linetrs

--- morse.rb
+++ morse.rb
@@ -1,13 +1,13 @@
 MORSE_CODE = {
-  '.-'   => 'A',   '-...' => 'B',   '-.-.' => 'C',
-  '-..'  => 'D',   '.'    => 'E',   '..-.' => 'F',
-  '--.'  => 'G',   '....' => 'H',   '..'   => 'I',
-  '.---' => 'J',   '-.-'  => 'K',   '.-..' => 'L',
-  '--'   => 'M',   '-.'   => 'N',   '---'  => 'O',
-  '.--.' => 'P',   '--.-' => 'Q',   '.-.'  => 'R',
-  '...'  => 'S',   '-'    => 'T',   '..-'  => 'U',
-  '...-' => 'V',   '.--'  => 'W',   '-..-' => 'X',
-  '-.--' => 'Y',   '--..' => 'Z'
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+  '-..' => 'D', '.'    => 'E', '..-.' => 'F',
+  '--.' => 'G', '....' => 'H', '..'   => 'I',
+  '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+  '...' => 'S', '-' => 'T', '..-' => 'U',
+  '...-' => 'V', '.--'  => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z'
 }.freeze
 
 def decode_char(char)
@@ -17,7 +17,7 @@ end
 def morse_code(string)
   decoded_text = []
   words = string.split('   ')
-  
+
   words.each do |word|
     new_word = word.split
     new_word.each do |single_word|
@@ -26,20 +26,20 @@ def morse_code(string)
     end
     decoded_text << ' '
   end
-  
+
   puts decoded_text.join
 end
 
 def decode_word(string)
   text = []
   word = string.split
-  
+
   word.each do |char|
     MORSE_CODE.each do |key, value|
       text << value if key == char
     end
   end
-  
+
   puts text.join
 end
 
@@ -50,4 +50,3 @@ morse_code('-- -.--   -. .- -- .   .. ...   -.. .- -. .. . .-..')
 decode_word('-.. .- -. .. . .-..')
 
 morse_code('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-
